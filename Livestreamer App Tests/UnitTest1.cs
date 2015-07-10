30d7baf84c7c5d15c3ba09c8c20ddb56e50c6a83
Jan Möller <jan.moeller0@gmail.com>
2015-07-10 18:26:14 +0200

    version 1.0 working

---
+++ Livestreamer App Tests/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Windows.Forms;
+using Livestreamer;
+
+namespace Livestreamer_App_Tests
+{
+    [TestClass]
+    public class LivestreamerTests
+    {
+        [TestMethod]
+        public void GetClipboardTest()
+        {
+            Core livestreamer = new Core();
+            Clipboard.SetText("hallo");
+            Assert.AreEqual("hallo", livestreamer.GetClipboard());
+            Clipboard.SetText("du hundesohne");
+            Assert.AreEqual("du hundesohne", livestreamer.GetClipboard());
+        }
+    }
+}
--- Project1/Class1.cs
+++ Project1/Class1.cs
@@ -3,15 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Windows.Forms;
 
 namespace Livestreamer
 {
-    class Livestreamer
+    class Program
     {
-        static int Main(string[] args)
+        [STAThread]
+        static int Main()
         {
-
+            Core livestreamer = new Core();
+            string url = livestreamer.GetClipboard();
+            livestreamer.StartLivestreamerProcess(url);
             return 0;
+        }
+
+        
+    }
+    public class Core
+    {
+        public string GetClipboard()
+        {
+            string clipboard = Clipboard.GetText();
+            return clipboard;
+        }
+        public void StartLivestreamerProcess(string url, string quality = "best")
+        {
+            Process process = new Process();
+            process.StartInfo = new ProcessStartInfo()
+            {
+                FileName = "livestreamer.exe",
+                Arguments = url + " " + quality
+            };
+            process.Start();
         }
     }
 }
